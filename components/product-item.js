c2e9d4a98304a9529e9e961ec234be6d79b5eb6d
TurtleNinja <nhatvytruong54@gmail.com>
2021-02-13 22:38:10 -0800

    done 1,2,4,5

--- components/product-item.js
+++ components/product-item.js
@@ -2,6 +2,44 @@
 
 class ProductItem extends HTMLElement {
   // TODO
+  constructor (){
+    super();
+
+    // create a shadow root
+    this.attachShadow({mode: 'open'});
+
+    // create a <li> product element
+    const wrapper = document.createElement('li');
+    wrapper.setAttribute('class', 'product');
+
+    // product image
+    const productImg = document.createElement('img');
+    productImg.setAttribute('width', '200');
+    productImg.src = this.hasAttribute('img') ? this.getAttribute('img') : "dummy";
+    wrapper.appendChild(productImg);
+
+    // title
+    const title = document.createElement('p');
+    title.setAttribute('class', 'title');
+    title.textContent = this.getAttribute('title');
+    wrapper.appendChild(title)
+
+    // price
+    const price = wrapper.appendChild(document.createElement('p'));
+    price.setAttribute('class', 'price');
+    price.textContent = this.getAttribute('price');
+    wrapper.appendChild(price);
+
+    // button
+    const button = document.createElement('button');
+    button.innerHTML = "Add to Cart";
+    button.addEventListener('click', () => {
+      button.innerHTML = (button.innerHTML === "Add to Cart") ? 'Remove from Cart' : "Add to Cart";
+    });
+    wrapper.appendChild(button);
+
+    this.shadowRoot.append(wrapper);
+  }
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,54 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  let myStorage = window.localStorage;
+  // fetch request to get data
+  fetch("https://fakestoreapi.com/products")
+    .then(response => response.json())
+    .then(data => {
+      // store the list in localStorage
+      let products = JSON.stringify(data);
+      if (myStorage.getItem('productList') !== products) {
+        myStorage.setItem('productList', products);
+      }
+      
+      // create a product component for each item
+      console.log(data, typeof(data));
+      for (let id in data) {
+        let item = data[id];
+        let productItem = document.createElement('product-item');
+        console.log(productItem);
+        productItem.setAttribute('title', item['title']);
+        productItem.setAttribute('price', item['price']);
+        productItem.setAttribute('img', item['image']);
+        
+        console.log(productItem);
+        //append to the product list
+        document.getElementById('product-list').appendChild(productItem);
+      }
+
+
+    })
+    .catch((error) => {console.log("Error: ", error);});
+
+  
+  // cart counting and toggle Add-Remove
+  let itemCount = 0;
+  document.querySelectorAll('button').forEach(function(btn) {
+    btn.addEventListener('click', () => {
+      console.log(btn.innerHTML);
+      if (btn.innerHTML === 'Add to Cart') {
+        itemCount++;
+        //btn.innerHTML = 'Remove from Cart';
+      }
+      else {
+        itemCount--;
+        //btn.innerHTML = 'Add to Cart';
+      }
+      document.getElementById('cart-count').innerHTML = itemCount;
+    });
+  });
+
+
+  // make sure cart is remembered
 });
