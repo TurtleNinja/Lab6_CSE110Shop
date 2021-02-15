03a4f8d60b6db00e607e00e97dcaf1cef7aa9bc5
TurtleNinja <nhatvytruong54@gmail.com>
2021-02-14 20:21:51 -0800

    fix product-item web component; edit button implementation; add cart info to localStorage

--- components/product-item.js
+++ components/product-item.js
@@ -1,45 +1,90 @@
 // product-item.js
 
 class ProductItem extends HTMLElement {
-  // TODO
+  
   constructor (){
     super();
-
     // create a shadow root
-    this.attachShadow({mode: 'open'});
-
-    // create a <li> product element
-    const wrapper = document.createElement('li');
-    wrapper.setAttribute('class', 'product');
-
-    // product image
-    const productImg = document.createElement('img');
-    productImg.setAttribute('width', '200');
-    productImg.src = this.hasAttribute('img') ? this.getAttribute('img') : "dummy";
-    wrapper.appendChild(productImg);
-
-    // title
-    const title = document.createElement('p');
-    title.setAttribute('class', 'title');
-    title.textContent = this.getAttribute('title');
-    wrapper.appendChild(title)
-
-    // price
-    const price = wrapper.appendChild(document.createElement('p'));
-    price.setAttribute('class', 'price');
-    price.textContent = this.getAttribute('price');
-    wrapper.appendChild(price);
-
-    // button
-    const button = document.createElement('button');
-    button.innerHTML = "Add to Cart";
-    button.addEventListener('click', () => {
-      button.innerHTML = (button.innerHTML === "Add to Cart") ? 'Remove from Cart' : "Add to Cart";
-    });
-    wrapper.appendChild(button);
-
-    this.shadowRoot.append(wrapper);
+    this.root = this.attachShadow({mode: 'open'});
   }
+
+  set item(item) {
+    this.id = item.id;
+    this.root.innerHTML = `
+      <li class="product">
+        <img src='${item.image}' alt='${item.title}'width='200'>
+        <p class='title'>${item.title}</p>
+        <p class='price'>$${item.price}</p>
+        <button name='${item.id}'>Add to Cart</button>
+      </li>
+      <style>
+      .price {
+        color: green;
+        font-size: 1.8em;
+        font-weight: bold;
+        margin: 0;
+      }
+      
+      .product {
+        align-items: center;
+        background-color: white;
+        border-radius: 5px;
+        display: grid;
+        grid-template-areas: 
+        'image'
+        'title'
+        'price'
+        'add';
+        grid-template-rows: 67% 11% 11% 11%;
+        height: 450px;
+        filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+        margin: 0 30px 30px 0;
+        padding: 10px 20px;
+        width: 200px;
+      }
+      
+      .product > button {
+        background-color: rgb(255, 208, 0);
+        border: none;
+        border-radius: 5px;
+        color: black;
+        justify-self: center;
+        max-height: 35px;
+        padding: 8px 20px;
+        transition: 0.1s ease all;
+      }
+      
+      .product > button:hover {
+        background-color: rgb(255, 166, 0);
+        cursor: pointer;
+        transition: 0.1s ease all;
+      }
+      
+      .product > img {
+        align-self: center;
+        justify-self: center;
+        width: 100%;
+      }
+      
+      .title {
+        font-size: 1.1em;
+        margin: 0;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+      
+      .title:hover {
+        font-size: 1.1em;
+        margin: 0;
+        white-space: wrap;
+        overflow: auto;
+        text-overflow: unset;
+      }
+      </style>
+    `
+  }
+
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,54 +1,95 @@
 // Script.js
+let myStorage = window.localStorage;
+let itemsInCart = [];
+let itemCount = 0;
+
+
+/**
+ * Load cart information from localStorage
+ */
+function loadCart() {
+  if (myStorage.getItem('inCart')) {
+    itemsInCart = myStorage.getItem('inCart').split(',');
+  }
+  else {
+    myStorage.setItem('inCart', itemsInCart);
+  }
+  itemCount = itemsInCart.length;
+  document.getElementById('cart-count').innerHTML = itemCount;
+}
+
+/**
+ * 
+ * @param {product-item} item 
+ * When the button is clicked, 
+ * If it is 'Add to Cart', increment the item count, push id to the cart array, and
+ * switch label to 'Remove from Cart'
+ * If it is 'Remove from Cart', decrement the item count, remove id to the cart array, and
+ * switch label to 'Add to Cart'
+ * 
+ */
+
+function addEventForButton(item) {
+  let btn = item.shadowRoot.querySelector('button');
+  btn.addEventListener('click', () => {
+    let itemNum = btn.getAttribute('name');
+
+    if (btn.innerHTML === 'Add to Cart') {
+      // add new item to cart
+      if (itemsInCart.indexOf(itemNum) === -1) {
+        itemsInCart.push(itemNum);
+        myStorage.setItem('inCart', itemsInCart);
+        itemCount++;
+      }
+      // alert if item is already in cart
+      else {
+        alert('Item was already added');
+      }
+      // change button label
+      btn.innerHTML = 'Remove from Cart';
+    }
+    else {
+      itemCount--;
+      // remove item from cart list
+      itemsInCart.splice(itemsInCart.indexOf(itemNum), 1);
+      myStorage.setItem('inCart', itemsInCart);
+      // change button label
+      btn.innerHTML = 'Add to Cart';
+    }
+
+    document.getElementById('cart-count').innerHTML = itemCount;
+  });
+}
+
 
 window.addEventListener('DOMContentLoaded', () => {
-  let myStorage = window.localStorage;
+  loadCart();
+
   // fetch request to get data
   fetch("https://fakestoreapi.com/products")
-    .then(response => response.json())
-    .then(data => {
-      // store the list in localStorage
+  .then(response => response.json())
+  .then(data => {
+
+      // store the product list in localStorage
       let products = JSON.stringify(data);
       if (myStorage.getItem('productList') !== products) {
         myStorage.setItem('productList', products);
       }
       
       // create a product component for each item
-      console.log(data, typeof(data));
       for (let id in data) {
         let item = data[id];
+
+        // create a product-item element
         let productItem = document.createElement('product-item');
-        console.log(productItem);
-        productItem.setAttribute('title', item['title']);
-        productItem.setAttribute('price', item['price']);
-        productItem.setAttribute('img', item['image']);
-        
-        console.log(productItem);
+        productItem.item = item;
+
+        // add functionality for button
+        addEventForButton(productItem);
+
         //append to the product list
         document.getElementById('product-list').appendChild(productItem);
       }
-
-
-    })
-    .catch((error) => {console.log("Error: ", error);});
-
-  
-  // cart counting and toggle Add-Remove
-  let itemCount = 0;
-  document.querySelectorAll('button').forEach(function(btn) {
-    btn.addEventListener('click', () => {
-      console.log(btn.innerHTML);
-      if (btn.innerHTML === 'Add to Cart') {
-        itemCount++;
-        //btn.innerHTML = 'Remove from Cart';
-      }
-      else {
-        itemCount--;
-        //btn.innerHTML = 'Add to Cart';
-      }
-      document.getElementById('cart-count').innerHTML = itemCount;
-    });
-  });
-
-
-  // make sure cart is remembered
+  })
+  .catch((error) => {console.log("Error: ", error)}); // end fetching data
 });
